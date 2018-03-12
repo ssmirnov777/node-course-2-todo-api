6638310aaab1841be24f7c9135332445bd9236f3
Sergey Smirnov <ssmirnov777@gmail.com>
2018-03-12 12:35:48 -0700

    get by id added

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const {mongoose} = require ('./db/mongoose');
 const {Todo}  = require ('./models/todo');
 const {User}  = require ('./models/user');
+const {ObjectID} = require('mongodb');
+
 
 const app = express();
 
@@ -30,6 +32,23 @@ app.get("/todos", (req, res) => {
   })
 });
 
+app.get ("/todos/:id", (req,res) => {
+  id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    console.log(`ID ${id} not found`);
+    res.status(404).send({});
+  } else {
+      Todo.findById(id).then((todo) => {
+      if (!todo) {
+          return res.status(404).send({});
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send({});
+    });
+  }
+
+});
 
 app.listen(3000, () => {
   console.log("Listen on port 3000");
